3483eef3fe74eca1f511ee92a97e840f462f180a
Andy <spam@mrplanlos.de>
2018-11-14 09:35:06 +0100

    Update install.sh

--- opencanary-installer/install.sh
+++ opencanary-installer/install.sh
@@ -3,16 +3,16 @@ cd /home/pi
 echo "updating raspian"
 sudo apt update
 sudo apt full-upgrade -y
-echo "disabling SSHD and samba"
+echo -e "\e[32mdisabling SSHD and samba\e[39m"
 sudo systemctl stop nmbd
 sudo systemctl disable smbd
 sudo systemctl disable ssh.service
 sudo update-rc.d nmbd disable
 
-echo "installing opencanary pre-requisites"
+echo -e "\e[32minstalling opencanary pre-requisites\e[39m"
 sudo apt install -y python-dev python-pip python-virtualenv libssl-dev libffi-dev libpcap-dev libpq-dev samba
-echo "creating virtualenv"
+echo -e "\e[32mcreating virtualenv\e[39m"
 mkdir canary-env
 virtualenv -p python2 canary-env
-echo "Now type: source /home/pi/canary-env/bin/activate"
-echo "After that within the virtualenv sh ./install-env.sh"
+echo -e "\e[32mNow type: source /home/pi/canary-env/bin/activate\e[39m"
+echo -e "\e[32mAfter that within the virtualenv sh ./install-env.sh\e[39m"
