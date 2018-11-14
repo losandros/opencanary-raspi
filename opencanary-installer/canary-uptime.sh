f2cea0f7df2160c28e2578b9813cd4305a4a1498
Andy <spam@mrplanlos.de>
2018-11-14 08:37:48 +0100

    Update uptime script
    added src-host field

--- opencanary-installer/canary-uptime.sh
+++ opencanary-installer/canary-uptime.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo '{"local_time": "'$(date +%Y-%m-%d\ %H:%M:%S)'", "logtype": "heartbeat", "node_id": "'$(hostname)'", "logdata": "'$(ps -ef |grep opencanaryd |grep -v grep)'"}' >> /var/tmp/opencanary-up.log 
+echo '{"local_time": "'$(date +%Y-%m-%d\ %H:%M:%S)'", "logtype": "1003", "node_id": "'$(hostname)'", "logdata": "'$(ps -ef |grep opencanaryd |grep -v grep)'", "src_host": "'$(ip addr | awk '/^[0-9]+/ { currentinterface=$2 } $1 == "inet" { split( $2, foo, "/" ); print currentinterface ,foo[1] }' | grep -v lo)'"}' >> /var/tmp/opencanary-up.log 
 
